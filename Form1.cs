a7bc5c6a828806c5592f49ce347e7738d446a6cb
Qinyi Cai <CaiQinyi@foxmail.com>
2018-01-07 13:51:32 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase
+{
+    public partial class SyntheticalExperimenta : Form
+    {
+        public SyntheticalExperimenta()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_StudentInformation_Click(object sender, EventArgs e)
+        {
+            StudentInformationManagement studentinformationmanagement = new StudentInformationManagement();
+            studentinformationmanagement.Location = new Point(this.Location.X + 100, this.Location.Y);
+            studentinformationmanagement.ShowDialog();
+        }
+
+        private void btn_StudentAchievement_Click(object sender, EventArgs e)
+        {
+            StudentAchievementManagement studentachievementmanagement = new StudentAchievementManagement();
+            studentachievementmanagement.ShowDialog();
+        }
+
+        private void btn_CourseMaterials_Click(object sender, EventArgs e)
+        {
+            CourseMaterialsManagement coursematerialsmanagement = new CourseMaterialsManagement();
+            coursematerialsmanagement.ShowDialog();
+        }
+    }
+}
